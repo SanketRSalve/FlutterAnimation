edcc3c0c2cb2a4a7a75dc0a4da7d009d848ccf89
DragonTrail99 <sanket.r.salve@gmail.com>
2023-09-16 00:59:01 +0530

    Heart Shape Rotating Animation

--- animation_1/lib/main.dart
+++ animation_1/lib/main.dart
@@ -24,6 +24,28 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// HeartShape Clipper
+class HeartClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    double x = size.width;
+    double y = size.height;
+
+    Path path = Path();
+    path.moveTo(0.5 * x, 0.35 * y);
+    path.cubicTo(0.2 * x, 0.1 * y, -0.25 * x, 0.6 * y, 0.5 * x, y);
+    path.moveTo(0.5 * x, 0.35 * y);
+    path.cubicTo(0.8 * x, 0.1 * y, 1.25 * x, 0.6 * y, 0.5 * x, y);
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -55,6 +77,7 @@ class _HomePageState extends State<HomePage>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFD5E1FE),
       body: Center(
         child: AnimatedBuilder(
           animation: _animation,
@@ -62,22 +85,25 @@ class _HomePageState extends State<HomePage>
             return Transform(
               alignment: Alignment.center,
               transform: Matrix4.identity()..rotateY(_animation.value),
-              child: Container(
-                height: 200,
-                width: 200,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15.0),
-                    boxShadow: const [
-                      BoxShadow(
-                          color: Color(0xffcff3fe),
-                          offset: Offset(0.5, 0.5),
-                          spreadRadius: 4.0,
-                          blurRadius: 10.0),
-                    ],
-                    gradient: const LinearGradient(
-                        colors: [Color(0xff2bbffd), Color(0xff4ae4e6)],
-                        begin: Alignment.centerLeft,
-                        end: Alignment.centerRight)),
+              child: ClipPath(
+                clipper: HeartClipper(),
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15.0),
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Color(0xffcff3fe),
+                            offset: Offset(0.5, 0.5),
+                            spreadRadius: 4.0,
+                            blurRadius: 10.0),
+                      ],
+                      gradient: const LinearGradient(
+                          colors: [Color(0xFFFCA3B5), Color(0xFFFACDD6)],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter)),
+                ),
               ),
             );
           },
